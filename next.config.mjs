08432b722de0eaa074abac3a7e9272c3c785ecb5
wheazzyfinesse <59867253+wheazzyfinesse@users.noreply.github.com>
2024-07-25 22:16:03 +0200

    new airbnb cloned website in nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    env: {
+        KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+        KINDE_POST_LOGOUT_REDIRECT_URL:
+            process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+        KINDE_POST_LOGIN_REDIRECT_URL:
+            process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`
+    },
     images: {
         remotePatterns: [{
             hostname: "a0.muscache.com",
@@ -13,3 +20,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
